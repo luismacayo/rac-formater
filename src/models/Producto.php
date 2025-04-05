d4756144c3504ea9e3f7e90311d02f3c7dff2bd4
Luis Macayo <luismacayo@gmail.com>
2025-04-04 22:47:08 -0500

    Update Producto.php

--- src/models/Producto.php
+++ src/models/Producto.php
@@ -8,6 +8,30 @@ class Producto
     public $nombre;
     public $precio;
     public $stock;
+    public $errors = [];
+
+    public function validate()
+    {
+        $valid = true;
+        $this->errors = [];
+
+        if (empty($this->nombre)) {
+            $this->errors['nombre'] = 'El nombre no puede estar vacío.';
+            $valid = false;
+        }
+
+        if ($this->precio <= 0) {
+            $this->errors['precio'] = 'El precio debe ser mayor que cero.';
+            $valid = false;
+        }
+
+        if ($this->stock < 0) {
+            $this->errors['stock'] = 'El stock no puede ser negativo.';
+            $valid = false;
+        }
+
+        return $valid;
+    }
 
     public function calcularTotal()
     {
